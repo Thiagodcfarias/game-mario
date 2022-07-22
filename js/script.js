b02ff6843f75b076bc40cc5ef99b9ab4775cdac3
Thiago Farias <thiagodcfarias1@gmail.com>
2022-07-22 01:24:11 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const marioCss = document.querySelector('.mario')
+
+const jump = () => {
+    marioCss.classList.add('jump')
+
+    setTimeout(() => {
+        marioCss.classList.remove('jump')
+    }, 500);
+}
+
+
+const enemyCss = document.querySelector('.enemy')
+document.addEventListener("keydown", jump)
+
+const loop = setInterval(() => {
+    const enemyPosition = enemyCss.offsetLeft
+    const marioPosition = +window.getComputedStyle(marioCss).bottom.replace('px','')
+    if(enemyPosition <= 120 && enemyPosition >= -50 && marioPosition <= 80){
+        enemyCss.style.animation = 'none'
+        enemyCss.style.left = `${enemyPosition}px`
+
+        marioCss.style.animation = 'none'
+        marioCss.style.bottom = `${marioPosition}px`
+        marioCss.src = "./css/images/game-over.png"
+        marioCss.style.width = '75px'
+        marioCss.style.marginLeft = '50px'
+
+        clearInterval(loop)
+    }
+},10)
